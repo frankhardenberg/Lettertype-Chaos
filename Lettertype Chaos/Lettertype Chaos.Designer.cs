ab94b47abdb1c2ffeff83cc181e9a7f0382edaad
Frank Otten <34128958+frankhardenberg@users.noreply.github.com>
2018-10-01 14:42:52 +0200

    Lettertype Chaos MASTER
    Master Update

--- Lettertype Chaos/Lettertype Chaos.Designer.cs
+++ Lettertype Chaos/Lettertype Chaos.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.Input = new System.Windows.Forms.RichTextBox();
-            this.Output = new System.Windows.Forms.RichTextBox();
             this.Convert = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -37,31 +36,17 @@
             // 
             this.Input.Location = new System.Drawing.Point(12, 12);
             this.Input.Name = "Input";
-            this.Input.Size = new System.Drawing.Size(776, 154);
+            this.Input.Size = new System.Drawing.Size(776, 193);
             this.Input.TabIndex = 0;
             this.Input.Text = "";
             this.Input.TextChanged += new System.EventHandler(this.Input_TextChanged);
             // 
-            // Output
-            // 
-            this.Output.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.Output.Location = new System.Drawing.Point(12, 284);
-            this.Output.MaximumSize = new System.Drawing.Size(776, 154);
-            this.Output.MinimumSize = new System.Drawing.Size(776, 154);
-            this.Output.Name = "Output";
-            this.Output.Size = new System.Drawing.Size(776, 154);
-            this.Output.TabIndex = 1;
-            this.Output.Text = "";
-            this.Output.TextChanged += new System.EventHandler(this.Output_TextChanged);
-            // 
             // Convert
             // 
             this.Convert.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Convert.Location = new System.Drawing.Point(12, 172);
+            this.Convert.Location = new System.Drawing.Point(12, 211);
             this.Convert.Name = "Convert";
-            this.Convert.Size = new System.Drawing.Size(776, 106);
+            this.Convert.Size = new System.Drawing.Size(776, 75);
             this.Convert.TabIndex = 2;
             this.Convert.Text = "Convert";
             this.Convert.UseVisualStyleBackColor = true;
@@ -71,9 +56,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 298);
             this.Controls.Add(this.Convert);
-            this.Controls.Add(this.Output);
             this.Controls.Add(this.Input);
             this.MaximizeBox = false;
             this.Name = "LettertypeChaos";
@@ -85,7 +69,6 @@
         #endregion
 
         private System.Windows.Forms.RichTextBox Input;
-        private System.Windows.Forms.RichTextBox Output;
         private System.Windows.Forms.Button Convert;
     }
 }
--- Lettertype Chaos/Lettertype Chaos.cs
+++ Lettertype Chaos/Lettertype Chaos.cs
@@ -15,47 +15,27 @@ namespace Lettertype_Chaos
         public LettertypeChaos()
         {
             InitializeComponent();
-
+            
             foreach (FontFamily f in System.Drawing.FontFamily.Families)
             {
                 Fonts.Add(new Font(f.Name, 13));
             }
         }
 
-        string InputString;
         Random Random = new Random();
         List<Font> Fonts = new List<Font>();
 
         private void Convert_Click(object sender, EventArgs e)
         {
-            int i = 0;
-            InputString = Input.Text;
-
-            foreach (var Character in InputString)
+            for (int i = 0; i < Input.Text.Length; i++)
             {
-                Output.Font = Fonts[Random.Next(i, Fonts.Count)];
-                i++;
+                Input.Select(i, 1);
+                Input.SelectionFont = Fonts[Random.Next(i, Fonts.Count)];                
             }
-
-            Output.Text = InputString;
         }
 
         private void Input_TextChanged(object sender, EventArgs e)
         {
-        }
-
-        private void Output_TextChanged(object sender, EventArgs e)
-        {
-        }
-
-        private void Input_KeyDown(object sender, KeyEventArgs e)
-        {
-            Input.Text = "Schrijf hier de tekst die je wilt transformeren!";
-
-            if (e.KeyCode == Keys.Enter)
-            {
-                Input.Text = String.Empty;
-            }
         }
     }
 }
